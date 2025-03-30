2df775d00d4a5ffeb054e49a0d2b75f5d26c51b1
alicefrogeri <alicerfogeri43@gmail.com>
2025-03-30 17:21:30 -0300

    projeto finalizado

--- js/app.js
+++ js/app.js
@@ -2,6 +2,16 @@ let amigos = [];
 
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
+    if (amigo.value == '') {
+        alert('Infrome o nome do amigo!');
+        return;
+    }
+
+    if(amigos.includes(amigo.value)) {
+        alert('Nome ja adicionado!');
+        return;
+    }
+
     let lista = document.getElementById('lista-amigos');
     amigos.push(amigo.value);
     if (lista.textContent == '') {
@@ -13,6 +23,12 @@ function adicionar() {
 }
 
 function sortear() {
+    if(amigos.length < 4) {
+        alert('Adicione pelo menos 4 amigos!');
+        return;
+    }
+
+
     embaralhar(amigos);
     let sorteio = document.getElementById('lista-sorteio');
 
@@ -27,9 +43,57 @@ function sortear() {
     }
 }
 
+function excluirAmigo(index) {
+    amigos.splice(index, 1);
+    atualizarLista();
+    atualizarSorteio();
+}
+
 function embaralhar(lista) {
     for (let indice = lista.length; indice; indice--) {
         const indiceAleatorio = Math.floor(Math.random() * indice);
         [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
     }
+}
+
+function atualizarSorteio() {
+    let sorteio = document.getElementById('lista-sorteio');
+    sorteio.innerHTML = '';
+}
+
+function atualizarLista() {
+    let lista = document.getElementById('lista-amigos');
+    lista.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+        // Cria um elemento de parágrafo para cada amigo
+        let paragrafo = document.createElement('p');
+        paragrafo.textContent = amigos[i];
+    
+        // Adiciona um evento de clique para excluir o amigo
+        paragrafo.addEventListener('click', function() {
+            excluirAmigo(i);
+        });
+
+
+        // Adiciona o parágrafo à lista
+        lista.appendChild(paragrafo);
+    }
+}
+
+
+// Função para remover duplicatas de um array
+function removerDuplicatas(array) {
+    return [...new Set(array)];
+}
+
+  // Testando a função com novaLista
+let novaListaSemDuplicatas = removerDuplicatas(novaLista);
+console.log("Remover duplicatas:", novaListaSemDuplicatas);
+
+
+function reiniciar() {
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
 }
